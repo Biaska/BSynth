db196072a4ff728a2ffa565e2a7d1d00b51ed2aa
biaska <biaska70@gmail.com>
2025-02-04 17:56:02 -0800

    Synth Type

--- leaf/synth.c
+++ leaf/synth.c
@@ -2,60 +2,94 @@
 #include <math.h>
 #include <stdlib.h>
 
-// Helper function to get frequency from MIDI note
-float midiToHz(int midiNote)
+// Helper function to convert MIDI note to frequency
+static float midiToHz(int midiNote)
 {
     return 440.0f * powf(2.0f, (midiNote - 69) / 12.0f);
+}
+
+// Function to initialize a specific oscillator type
+static void synth_setOscillatorType(Synth *synth, OscillatorType oscType)
+{
+    // Assign function pointers based on the oscillator type
+    switch (oscType)
+    {
+    case OSC_SINE:
+        synth->tick = (OscillatorTickFn)tCycle_tick;
+        synth->init = (OscillatorInitFn)tCycle_init;
+        synth->free = (OscillatorFreeFn)tCycle_free;
+        synth->setFreq = (OscillatorSetFreqFn)tCycle_setFreq;
+        synth->setPhase = (OscillatorSetSampleRateFn)tCycle_setSampleRate;
+        synth->setSample = (OscillatorSetPhaseFn)tCycle_setPhase;
+        break;
+    case OSC_SQUARE:
+        synth->tick = (OscillatorTickFn)tSquare_tick;
+        synth->init = (OscillatorInitFn)tSquare_init;
+        synth->free = (OscillatorFreeFn)tSquare_free;
+        synth->setFreq = (OscillatorSetFreqFn)tSquare_setFreq;
+        synth->setPhase = (OscillatorSetSampleRateFn)tSquare_setSampleRate;
+        synth->setSample = (OscillatorSetPhaseFn)tSquare_setPhase;
+        break;
+    case OSC_SAW:
+        synth->tick = (OscillatorTickFn)tSawtooth_tick;
+        synth->init = (OscillatorInitFn)tSawtooth_init;
+        synth->free = (OscillatorFreeFn)tSawtooth_free;
+        synth->setFreq = (OscillatorSetFreqFn)tSawtooth_setFreq;
+        synth->setPhase = (OscillatorSetSampleRateFn)tSawtooth_setSampleRate;
+        synth->setSample = (OscillatorSetPhaseFn)tSawtooth_setPhase;
+        break;
+
+    default:
+        return;
+    }
+
+    void *type;
+
+    // Initialize all voices with the selected oscillator type
+    for (int i = 0; i < MAX_VOICES; i++)
+    {
+        if (oscType == OSC_SINE)
+        {
+            tCycle type;
+        } else if (oscType == OSC_SQUARE)
+        {
+            tSquare type;
+        } else if (oscType == OSC_SAW) {
+            tSawtooth type;
+        }
+        synth->oscillators[i] = type;
+        synth->init(synth->oscillators[i], synth->leaf);
+    }
 }
 
 // Initialize the synthesizer
 void synth_init(Synth *synth, LEAF *leaf, int sampleRate, OscillatorType oscType)
 {
     synth->leaf = leaf;
-    synth->sampleRate = sampleRate;
-    synth->oscType = oscType;
     tSimplePoly_init(&synth->poly, MAX_VOICES, leaf);
-
-    // Initialize oscillators based on the selected type
     synth_setOscillatorType(synth, oscType);
 }
 
-// Change oscillator type
-void synth_setOscillatorType(Synth *synth, OscillatorType oscType)
+// Free synth from mempool memory
+void synth_free(Synth *synth)
 {
-    synth->oscType = oscType;
-
-    for (int i = 0; i < MAX_VOICES; i++)
-    {
-        if (synth->oscillators[i])
-        {
-            free(synth->oscillators[i]); // Free old oscillator
-        }
-
-        if (oscType == OSC_SINE)
-        {
-            tCycle *osc = (tCycle *)malloc(sizeof(tCycle));
-            tCycle_init(osc, synth->leaf);
-            synth->oscillators[i] = osc;
-            synth->tickFunc[i] = (OscillatorTickFunc)tCycle_tick;
-        }
-        // Add square, saw oscillators here when implemented
-    }
+    // TODO
 }
 
-// Note On: Assigns note to a voice
+// Handle MIDI Note On
 void synth_noteOn(Synth *synth, int note, uint8_t velocity)
 {
     int voice = tSimplePoly_noteOn(&synth->poly, note, velocity);
-    if (voice >= 0)
+    if (voice >= 0 && synth->setFreq)
     {
         float freq = midiToHz(note);
-        tCycle_setFreq((tCycle *)synth->oscillators[voice], freq);
+        synth->setFreq(synth->oscillators[voice], freq);
     }
 }
 
-// Note Off: Removes note from the polyphony handler
+// Handle MIDI Note Off
 void synth_noteOff(Synth *synth, int note)
 {
-    tSimplePoly_noteOff(&synth->poly, note);
+    int voice = tSimplePoly_noteOff(&synth->poly, note);
+    synth->setFreq(synth->oscillators[voice], 0);
 }
--- leaf/synth.h
+++ leaf/synth.h
@@ -5,33 +5,40 @@
 
 #define MAX_VOICES 8
 
+// Enum for oscillator types
+typedef enum
+{
+    OSC_SINE,
+    OSC_SQUARE,
+    OSC_SAW
+} OscillatorType;
+
 // Function pointer for oscillator tick function
 typedef float (*OscillatorTickFn)(void *osc);
 typedef void  (*OscillatorFreeFn)(void *osc);
 typedef void  (*OscillatorInitFn)(void *osc, LEAF *leaf);
 typedef void  (*OscillatorSetFreqFn)(void *osc, float freq);
-typedef void  (*OscillatorSetSampleFn)(void *osc, float sr);
+typedef void  (*OscillatorSetSampleRateFn)(void *osc, float sr);
 typedef void  (*OscillatorSetPhaseFn)(void *osc, float phase);
 
 // Struct for voice handling
 typedef struct
 {
-    LEAF *leaf;                         // LEAF instance
-    tSimplePoly poly;                   // Poly Handler
-    void *oscillators[MAX_VOICES];      // Generic oscillator pointers
-    OscillatorTickFn tick;              // Tick function for specific osc
-    void *oscType;                      // Osc type
-    OscillatorFreeFn free;              // Osc free function
-    OscillatorInitFn init;              // Osc init function
-    OscillatorSetSampleFn setSample;    // Osc set sample function
-    OscillatorSetFreqFn setFreq;        // Osc set freq function
-    OscillatorSetPhaseFn setPhase;      // Osc set phase function
+    LEAF *leaf;                             // LEAF instance
+    tSimplePoly poly;                       // Poly Handler
+    void *oscillators[MAX_VOICES];          // Generic oscillator pointers
+    OscillatorTickFn tick;                  // Tick function for specific osc
+    OscillatorFreeFn free;                  // Osc free function
+    OscillatorInitFn init;                  // Osc init function
+    OscillatorSetSampleRateFn setSample;    // Osc set sample function
+    OscillatorSetFreqFn setFreq;            // Osc set freq function
+    OscillatorSetPhaseFn setPhase;          // Osc set phase function
 } Synth;
 
 // Fix these functions
 
-void synth_init(Synth *synth, LEAF *leaf, int sampleRate, OscillatorType oscType);
-void synth_setOscillatorType(Synth *synth, OscillatorType oscType);
+void synth_init(Synth *synth, LEAF *leaf, int sampleRate, void *oscType);
+void synth_setOscillatorType(Synth *synth, void *oscType);
 void synth_noteOn(Synth *synth, int note, uint8_t velocity);
 void synth_noteOff(Synth *synth, int note);
 
